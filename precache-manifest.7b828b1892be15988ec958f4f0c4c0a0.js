382bf088e2e0c06f8cb3df5d51928f904114ec00
rishav <rishavdps99@gmail.com>
2020-07-19 01:35:19 +0530

    Updates

---
+++ precache-manifest.7b828b1892be15988ec958f4f0c4c0a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29d6ba61d66aa66fb023c341dedb472",
+    "url": "/todoApplication/index.html"
+  },
+  {
+    "revision": "4b3cdf3849c9132130b6",
+    "url": "/todoApplication/static/css/main.6c901c8c.chunk.css"
+  },
+  {
+    "revision": "b4bd109735cf90239eeb",
+    "url": "/todoApplication/static/js/2.1907a7da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoApplication/static/js/2.1907a7da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b3cdf3849c9132130b6",
+    "url": "/todoApplication/static/js/main.94c81d03.chunk.js"
+  },
+  {
+    "revision": "e4bc942515291e531979",
+    "url": "/todoApplication/static/js/runtime-main.a16dbafb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todoApplication/static/media/logo.5d5d9eef.svg"
+  }
+]);
